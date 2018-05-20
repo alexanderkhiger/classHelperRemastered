01f725e3617ed73066c3449d9086e74929a47200
Alexander Khiger <alex10011997@gmail.com>
2018-05-20 17:17:00 +0400

    fix

--- classHelperRemastered/Views/mainwindowview.h
+++ classHelperRemastered/Views/mainwindowview.h
@@ -3,8 +3,7 @@
 
 #include <QMainWindow>
 
-class MainWindowView : public QMainWindow
-{
+class MainWindowView : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindowView(QString uID, QString uName, QString uShortname, QWidget *ref, QWidget *parent = 0);
