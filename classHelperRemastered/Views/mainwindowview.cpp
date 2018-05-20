6b13a4ebb69dd0273f42d2a51a9d9dc66f7a5fb9
Alexander Khiger <alex10011997@gmail.com>
2018-05-20 17:20:15 +0400

    fix

--- classHelperRemastered/Views/mainwindowview.cpp
+++ classHelperRemastered/Views/mainwindowview.cpp
@@ -1,6 +1,5 @@
 #include "mainwindowview.h"
 
-MainWindowView::MainWindowView(QString uID, QString uName, QString uShortname, QWidget *ref, QWidget *parent) : QMainWindow(parent)
-{
+MainWindowView::MainWindowView(QString uID, QString uName, QString uShortname, QWidget *ref, QWidget *parent) : QMainWindow(parent) {
 
 }
