de8e52a5e5c37f47f023e0cf94644f0503abac11
Alexander Khiger <alex10011997@gmail.com>
2018-05-26 15:51:00 +0400

    some additions

--- classHelperRemastered/Views/mainwindowview.cpp
+++ classHelperRemastered/Views/mainwindowview.cpp
@@ -1,5 +1,94 @@
 #include "mainwindowview.h"
 
-MainWindowView::MainWindowView(QString uID, QString uName, QString uShortname, QWidget *ref, QWidget *parent) : QMainWindow(parent) {
+MainWindowView::MainWindowView(QString uID, QString uName, QString uShortname, QWidget *parent) {
+    this->parent = parent;
+    receivedID = uID;
+    receivedName = uName;
+    receivedShortname = uShortname;
+    createUI();
+}
+
+void MainWindowView::createUI() {
+    leftBar = new QToolBar;
+    leftBar->setMovable(false);
+    leftBar->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+
+    QToolButton *distributionTool = new QToolButton;
+    QToolButton *tableEditorTool = new QToolButton;
+    QToolButton *outputTool = new QToolButton;
+    QToolButton *fileProcessingTool = new QToolButton;
+
+    distributionTool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    distributionTool->setIcon(style()->standardIcon(QStyle::SP_FileDialogContentsView));
+    distributionTool->setText(tr("Нагрузка"));
+    distributionTool->setFixedSize(70,60);
+    distributionTool->setToolTip(tr("Распределение нагрузки по преподавателям"));
+    connect(distributionTool,SIGNAL(clicked(bool)),this,SLOT(setWorkFieldAsCentral()));
+
+    tableEditorTool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    tableEditorTool->setIcon(style()->standardIcon(QStyle::SP_FileDialogDetailedView));
+    tableEditorTool->setText(tr("Редактор"));
+    tableEditorTool->setFixedSize(70,60);
+    tableEditorTool->setToolTip(tr("Редактор таблиц (словарей)"));
+    connect(tableEditorTool,SIGNAL(clicked(bool)),this,SLOT(setTableEditorAsCentral()));
+
+    outputTool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    outputTool->setIcon(style()->standardIcon(QStyle::SP_FileIcon));
+    outputTool->setText(tr("Отчеты"));
+    outputTool->setFixedSize(70,60);
+    outputTool->setToolTip(tr("Получение отчета"));
+    connect(outputTool,SIGNAL(clicked(bool)),this,SLOT(setOutputToFileAsCentral()));
+
+    fileProcessingTool->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    fileProcessingTool->setIcon(style()->standardIcon(QStyle::SP_FileIcon));
+    fileProcessingTool->setText(tr("Обработка"));
+    fileProcessingTool->setFixedSize(70,60);
+    fileProcessingTool->setToolTip(tr("Обработка файла"));
+    connect(fileProcessingTool,SIGNAL(clicked(bool)),this,SLOT(setFileProcessingAsCentral()));
+
+    leftBar->addWidget(distributionTool);
+    leftBar->addWidget(tableEditorTool);
+    leftBar->addWidget(outputTool);
+    leftBar->addWidget(fileProcessingTool);
+
+    this->addToolBar(Qt::LeftToolBarArea,leftBar);
+
+    setMinimumHeight(500);
+    setMinimumWidth(500);
+
+    fileMenu = new QMenu(tr("Файл"));
+
+    this->menuBar()->addMenu(fileMenu);
+
+    backToUniversityList = new QAction(tr("Выбор университета"), this);
+    backToUniversityList->setStatusTip(tr("Назад к выбору университета"));
+    fileMenu->addAction(backToUniversityList);
+
+    connect(backToUniversityList,SIGNAL(triggered(bool)),this,SLOT(back()));
+
+    setWindowTitle(QString(tr("Time Tracker | Название университета: %1 | Аббревиатура: %2")).arg(receivedName).arg(receivedShortname));
 
 }
+
+void MainWindowView::back() {
+    parent->show();
+    close();
+}
+
+void MainWindowView::setWorkFieldAsCentral() {
+
+}
+
+void MainWindowView::setTableEditorAsCentral() {
+
+}
+
+void MainWindowView::setOutputToFileAsCentral() {
+
+}
+
+void MainWindowView::setFileProcessingAsCentral() {
+
+}
+
+
--- classHelperRemastered/Views/mainwindowview.h
+++ classHelperRemastered/Views/mainwindowview.h
@@ -2,15 +2,42 @@
 #define MAINWINDOWVIEW_H
 
 #include <QMainWindow>
+#include <QToolBar>
+#include <QToolButton>
+#include <QStyle>
+#include <QMenu>
+#include <QMenuBar>
 
 class MainWindowView : public QMainWindow {
     Q_OBJECT
 public:
-    explicit MainWindowView(QString uID, QString uName, QString uShortname, QWidget *ref, QWidget *parent = 0);
+    explicit MainWindowView(QString uID, QString uName, QString uShortname, QWidget *parent);
 
 signals:
 
+private slots:
+    void createUI();
+    void back();
+    void setWorkFieldAsCentral();
+    void setTableEditorAsCentral();
+    void setOutputToFileAsCentral();
+    void setFileProcessingAsCentral();
+
 public slots:
+
+private:
+
+    QString receivedID;
+    QString receivedName;
+    QString receivedShortname;
+    QWidget *parent;
+
+    QToolBar *leftBar;
+
+    QMenu *fileMenu;
+
+    QAction *backToUniversityList;
+
 };
 
 #endif // MAINWINDOWVIEW_H
